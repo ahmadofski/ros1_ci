23ccf934f00037ede95e638ed01a3e70cddcee9a
ahmadofski <46104490+ahmadofski@users.noreply.github.com>
2025-02-25 11:21:07 +0000

    Update test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 source /opt/ros/noetic/setup.bash
-cd /catkin_ws
-source devel/setup.bash && rostest -r tortoisebot_waypoints waypoints_test.test
+cd /catkin_ws && source devel/setup.bash && rostest -r tortoisebot_waypoints waypoints_test.test
